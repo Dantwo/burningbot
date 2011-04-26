2e10af1c066365b4b1dd89d6b52a893e4e7ca544
Andrew Brehaut <andrew@brehaut.net>
2011-04-27 08:29:29 +1200

    fixes glitches in dice

--- src/burningoracle/dice.clj
+++ src/burningoracle/dice.clj
@@ -87,7 +87,7 @@
       (swap! users-last-explodables dissoc nick)
       (when (not (:exploded? dice))
         (let [to-explode (count (filter #(= % 6) rolled))]
-          (when [(> to-explode 0)]
+          (when (> to-explode 0)
             (let [new-result (roll {:exploding? true
                                     :shade (:shade dice)
                                     :exponent to-explode})
