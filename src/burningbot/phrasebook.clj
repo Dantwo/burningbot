1a0d4cf6aa3b787a2ff91dddf5380752ce07c5fc
Andrew Brehaut <andrew@brehaut.net>
2011-05-03 20:05:51 +1200

    phrasebook looks up settings for priviledged users rahter than hardcoded value

--- src/burningbot/phrasebook.clj
+++ src/burningbot/phrasebook.clj
@@ -1,41 +1,51 @@
 (ns burningbot.phrasebook
-  (:require [clojure.java.io :as io]))
+  "This is one of the oldest modules in burningbot. it stores messages by key and retrieves them again.
+   it is desperately in need of being rewritten. This will probably occur once some sql approach has
+   been decided on.
 
+   features for the future:
+     * should hook into a general keyword lookup feature so that key finding logic is generalised
+     * provide tools for multiple possible answers
+     * responses with 'key _is_ response' and optionally 'key _are_ response' responses (similar to clojurebot)
+     * allow keys up to three words"
+  
+  (:require [clojure.java.io :as io]
+            [burningbot.settings :as settings]))
 
 (def phrasebook-url "resources/phrasebook")
 
 (def canned-phrases
-  (atom
+  (ref
    (read-string (slurp phrasebook-url))))
 
 (defonce phrasebook-agent (agent nil))
 
-(defn save-phrasebook! []
+(defn save-phrasebook []
   (send phrasebook-agent (fn [_] (spit phrasebook-url (prn-str @canned-phrases)))))
 
-(defn forget!
+(defn forget
   [key]
-  (swap! canned-phrases
-         dissoc key)
-  (save-phrasebook!))
+  (dosync
+   (alter canned-phrases
+           dissoc key)
+   (save-phrasebook)))
 
-(defn learn!
+(defn learn
   [key message]
   (when-let [[_ response] (re-matches #"^\S+\s+is\s+(.+)$"
                                       message)]
-    (swap! canned-phrases assoc key response)
-    (save-phrasebook!)))
-
-(def priviledged-users #{"brehaut" "Zelbinian" "cathexis" "oliof"})
+    (dosync
+     (alter canned-phrases assoc key response)
+     (save-phrasebook))))
 
 (defn handle-learn-phrase [{:keys [nick pieces message]}]
   (prn nick pieces message)
-  (when (contains? priviledged-users nick)
+  (when (contains? (settings/read-setting [:phrasebook :priviledged-users] #{}) nick)
     (cond (= "forget" (first pieces)) (let [key (second pieces)]
-                                        (forget! key)
+                                        (forget key)
                                         (str key "? nope, never heard of it."))
           (= "is" (second pieces)) (do
-                                     (learn! (first pieces) message)
+                                     (learn (first pieces) message)
                                      "sure thing boss."))))
 
 (defn handle-canned
